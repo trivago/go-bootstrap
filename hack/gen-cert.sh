7a1495f11f0638f9e7be069d3f60c2c3d6c8b164
Arne Claus <arne.claus@trivago.com>
2023-12-01 14:49:27 +0100

    fix self signed cert

--- hack/gen-cert.sh
+++ hack/gen-cert.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-openssl req -new -subj "/C=US/ST=Utah/CN=localhost" -newkey rsa:2048 -nodes -keyout tls.key -out localhost.csr
-openssl x509 -req -days 3650 -in localhost.csr -signkey tls.key -out tls.cert
+echo 'Generating selfsign root CA'
+openssl req -x509 -nodes -new -sha256 -days 3650 -newkey rsa:2048 -keyout root-ca.key -out root-ca.pem -subj "/C=DE/ST=NRW/O=trivago/CN=localhost"
+openssl x509 -outform pem -in root-ca.pem -out root-ca.crt
+
+echo 'Generating server cert'
+openssl req -new -nodes -newkey rsa:2048 -subj "/C=DE/ST=NRW/L=Duesseldorf/O=trivago/CN=localhost" -keyout tls.key -out localhost.csr
+openssl x509 -req -sha256 -days 3650 -in localhost.csr -CA root-ca.pem -CAkey root-ca.key -CAcreateserial -extfile domains.ext -out tls.cert
